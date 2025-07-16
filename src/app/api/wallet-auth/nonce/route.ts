7a280b374db9b2d50988391702cb628b8401cf64
James <jf358950@gmail.com>
2025-07-16 23:51:28 +0100

    Initial commit

--- src/app/api/wallet-auth/nonce/route.ts
+++ src/app/api/wallet-auth/nonce/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
 import crypto from 'crypto';
-import { hashNonce } from '@/auth/wallet/client-helpers';
+
+const hashNonce = ({ nonce }: { nonce: string }) => {
+  const hmac = crypto.createHmac('sha256', process.env.HMAC_SECRET_KEY!);
+  hmac.update(nonce);
+  return hmac.digest('hex');
+};
 
 export async function POST() {
   const nonce = crypto.randomUUID().replace(/-/g, '');
